fc1cf835480cae6e3042a254d471058e629c0798
Yasin Jafari <yasinj7658@gmail.com>
2024-07-10 21:59:51 +0330

    update app.ts for adding Body Parsing Middleware

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,21 @@
 import express, { Application } from "express";
 import connectDB from "./config/db";
 import bodyParser from "body-parser";
+import dietPlanRoutes from "./routes/dietPlanRoutes";
 import dotenv from "dotenv";
 dotenv.config();
-
 const app: Application = express();
+
+app.use(express.json());
 
 // Connect to database
 connectDB();
 
-// Middleware
-app.use(bodyParser.json());
+// Routes
+app.use("/api/diet-plans", dietPlanRoutes);
+
+// // Middleware
+// app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
 
